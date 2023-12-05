5cf635bbab46dfb9c3fddd92744c2aced2c6e1a8
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-12-05 17:17:19 +0900

    reset: restart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import Intro from '@/layouts/Intro';
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p className="text-3xl font-bold underline">
-        Helaaaaaaaaaaaaalo wasdasdaaorasdassdfsdld!
-      </p>
-      <Intro />
+      <div className="bg-rose-700 p-3 pt-1">asdasd</div>
+      <div className="">sdfsdfs</div>
     </>
   );
 }
