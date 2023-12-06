0319dd23d7237201fc92deb7eed8f1c6cf659925
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-12-06 17:02:10 +0900

    design:applied googlefont

--- src/components/ContentArea.tsx
+++
@@ -1,9 +0,0 @@
-const ContentArea = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <section className="h-screen w-screen border-2 border-red-600 bg-slate-400">
-      {children}
-    </section>
-  );
-};
-
-export default ContentArea;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        lalezar: ['Lalezar'],
+        knewave: ['Knewave'],
+      },
     },
   },
   plugins: [],
